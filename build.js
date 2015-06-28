2c5e200f651c79aaaf2f460bd685c16be11a3fda
M. Adam Kendall <linuxbozo@gmail.com>
2015-06-28 16:45:01 -0400

    Update repo build script

--- build.js
+++ build.js
@@ -2,13 +2,22 @@
 
 var GitHubApi = require('github'),
     Promise = require('bluebird'),
+    moment = require('moment'),
     _ = require('lodash');
 
 var github = new GitHubApi({
     version: '3.0.0',
     //debug: true,
-    protocol: 'https'
+    protocol: 'https',
+    headers: {
+        'user-agent': 'LinuxBozo-ads-app'
+    }
 });
+github.authenticate({
+    type: 'token',
+    token: '311b94ea28ad23fa8d8796c0bb8f5e1823effd3b'
+});
+
 
 function BPAData() {
     this.matchingRepos = {};
@@ -31,7 +40,7 @@ BPAData.prototype.doSearch = function(terms) {
         _.map(data.items, function(repo) {
             // We haven't seen a repo from this owner yet.
             var notFound = Object.keys(self.matchingRepos).indexOf(repo.owner.login) === -1;
-            var outdated = self.matchingRepos[repo.owner.login] && self.matchingRepos[repo.owner.login].pushed_at < repo.pushed_at;
+            var outdated = !notFound && moment(self.matchingRepos[repo.owner.login].pushed_at) < moment(repo.pushed_at);
             if (notFound || outdated) {
                 self.matchingRepos[repo.owner.login] = repo;
             }
@@ -41,14 +50,17 @@ BPAData.prototype.doSearch = function(terms) {
 
 var data = new BPAData();
 var terms = [
-    '4QTFHS150004 OR RFQ993471 OR "agile delivery" OR 18F',
-    'BPA NOT crypto NOT australia',
+    '4QTFHS150004 OR RFQ993471 OR "agile delivery"',
+    '18F NOT microcontroller NOT "bad code" NOT font NOT laptop',
+    'BPA NOT crypto NOT australia NOT boot',
     'GSA ADS',
     'agile GSA',
-    'open FDA',
-    'FDA food',
-    'FDA enforcement',
-    'FDA recall',
+    'openFDA',
+    'open FDA NOT illustration',
+    'FDA agile',
+    'FDA food NOT koop',
+    'FDA enforcement NOT koop',
+    'FDA recall NOT koop',
     'FDA device',
     'FDA drug',
 ];
