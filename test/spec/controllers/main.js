7487d3a90cdb7554723d380fa16438b88dfe8490
M. Adam Kendall <linuxbozo@gmail.com>
2015-06-29 20:34:47 -0400

    Fix tests

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -16,7 +16,7 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should set initial scope', function () {
+    expect(scope.order).toBe('-pushed_at');
   });
 });
